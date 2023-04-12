01816897d977fc96d5cdd6c7cf2d5be244d0513e
GiacomoArena <giacomoarena28@gmail.com>
2023-04-12 14:44:32 +0200

    update commit

--- js/script.js
+++ js/script.js
@@ -6,7 +6,18 @@ createApp({
     return{
       outputMessage: "Ciao questo é il mio primo Vue" ,
       image: '<img src="img/download.png">',
+      counter: 0,
+    }
+  },
+
+
+methods:{
+  addCounter(){
+    this.counter++;
+    if (this.counter >= 10) {
+      this.counter = "ciao"
     }
   }
+}
 
 }).mount("#app")
